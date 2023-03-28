341b1815c9f00a3173e13d3406e5205eb64eabef
Mon-Martinez <126629463+Mon-Martinez@users.noreply.github.com>
2023-03-27 22:38:11 -0500

    Added list of all components and imports to apps.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import Header from './components/Header';
+import Description from './components/Description';
+import Instructions from './components/Instructions';
+import DaysButton from './components/DaysButton';
+import DropDown from './components/DropDown';
+import Input from './components/Input';
+import MoodOverview from './components/MoodOverview';
 
 function App() {
   return (
     <>
+    <Header />
+    <Description />
+    <Instructions />
+    <DaysButton />
+    <DropDown />
+    <Input />
+    <MoodOverview />
     </>
   );
 }
